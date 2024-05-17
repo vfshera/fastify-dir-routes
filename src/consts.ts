bc5f55c67f55730e0c4955897cd33ef6851e7c96
Franklin Shera <fshera96@gmail.com>
2024-05-17 08:40:06 +0300

    feat: add consts and types

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+export const ALLOWED_EXTENSIONS = [".ts", ".js"];
+export const ALLOWED_ROUTE_FILE = "route";
+
+/**
+ * a route with no directory after ()
+ */
+export const HANGING_ROUTE = `)/${ALLOWED_ROUTE_FILE}`;
---
+++ src/types.ts
@@ -0,0 +1,60 @@
+import type {
+  FastifyInstance,
+  RawServerBase,
+  RawServerDefault,
+  RawRequestDefaultExpression,
+  RawReplyDefaultExpression,
+  ContextConfigDefault,
+  RouteShorthandOptions,
+  FastifyRequest,
+  FastifyReply,
+} from "fastify";
+
+import type { RouteGenericInterface } from "fastify/types/route";
+
+type DirRouteHandlerMethod<
+  RawServer extends RawServerBase = RawServerDefault,
+  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
+  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>,
+  RouteGeneric extends RouteGenericInterface = RouteGenericInterface,
+  ContextConfig = ContextConfigDefault
+> = (
+  this: FastifyInstance<RawServer, RawRequest, RawReply>,
+  request: FastifyRequest<RouteGeneric, RawServer, RawRequest>,
+  reply: FastifyReply<
+    RawServer,
+    RawRequest,
+    RawReply,
+    RouteGeneric,
+    ContextConfig
+  >,
+  server: FastifyInstance<RawServer, RawRequest, RawReply>
+  // biome-ignore lint/suspicious/noConfusingVoidType: <explanation>
+) => void | RouteGeneric["Reply"] | Promise<RouteGeneric["Reply"] | void>;
+
+export interface RouteHandler<
+  RouteGeneric extends RouteGenericInterface = RouteGenericInterface,
+  ContextConfig = ContextConfigDefault,
+  RawServer extends RawServerBase = RawServerDefault,
+  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
+  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>
+> extends DirRouteHandlerMethod<
+    RawServer,
+    RawRequest,
+    RawReply,
+    RouteGeneric,
+    ContextConfig
+  > {
+  opts?: RouteShorthandOptions<
+    RawServer,
+    RawRequest,
+    RawReply,
+    RouteGeneric,
+    ContextConfig
+  >;
+}
+
+export type DirRoutesPluginOptions = {
+  routesDir: string;
+  prefix?: string;
+};
