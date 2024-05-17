01516bf86cae4a081e974b0cd2f757b1a0f061d6
Franklin Shera <fshera96@gmail.com>
2024-05-17 20:14:35 +0300

    fix: export types

--- src/index.ts
+++ src/index.ts
@@ -7,57 +7,59 @@ import kleur from "kleur";
 import type { DirRoutesPluginOptions } from "./types";
 import { makeError, registerRoutes } from "./utils";
 
+export * from "./types";
+
 const DirRoutesPlugin: FastifyPluginAsync<DirRoutesPluginOptions> = async (
-  fastify,
-  options
+	fastify,
+	options,
 ) => {
-  if (!options.routesDir) {
-    const message: string = makeError("dir must be specified");
-    throw new Error(kleur.red(message));
-  }
+	if (!options.routesDir) {
+		const message: string = makeError("dir must be specified");
+		throw new Error(kleur.red(message));
+	}
 
-  if (typeof options.routesDir !== "string") {
-    const message: string = makeError(
-      "dir must be the path of file system routes directory"
-    );
-    throw new Error(kleur.red(message));
-  }
+	if (typeof options.routesDir !== "string") {
+		const message: string = makeError(
+			"dir must be the path of file system routes directory",
+		);
+		throw new Error(kleur.red(message));
+	}
 
-  let dirPath: string;
+	let dirPath: string;
 
-  if (path.isAbsolute(options.routesDir)) {
-    dirPath = options.routesDir;
-  } else if (path.isAbsolute(process.argv[1] as string)) {
-    dirPath = path.join(process.argv[1] as string, "..", options.routesDir);
-  } else {
-    dirPath = path.join(
-      process.cwd(),
-      process.argv[1] as string,
-      "..",
-      options.routesDir
-    );
-  }
+	if (path.isAbsolute(options.routesDir)) {
+		dirPath = options.routesDir;
+	} else if (path.isAbsolute(process.argv[1] as string)) {
+		dirPath = path.join(process.argv[1] as string, "..", options.routesDir);
+	} else {
+		dirPath = path.join(
+			process.cwd(),
+			process.argv[1] as string,
+			"..",
+			options.routesDir,
+		);
+	}
 
-  if (!fs.existsSync(dirPath)) {
-    const message: string = makeError(`dir ${dirPath} does not exists`);
-    throw new Error(kleur.red(message));
-  }
+	if (!fs.existsSync(dirPath)) {
+		const message: string = makeError(`dir ${dirPath} does not exists`);
+		throw new Error(kleur.red(message));
+	}
 
-  if (!fs.statSync(dirPath).isDirectory()) {
-    const message: string = makeError(`dir ${dirPath} must be a directory`);
-    throw new Error(kleur.red(message));
-  }
+	if (!fs.statSync(dirPath).isDirectory()) {
+		const message: string = makeError(`dir ${dirPath} must be a directory`);
+		throw new Error(kleur.red(message));
+	}
 
-  try {
-    await registerRoutes(fastify, dirPath, options.prefix ?? "");
-  } catch (error) {
-    let msg = "";
+	try {
+		await registerRoutes(fastify, dirPath, options.prefix ?? "");
+	} catch (error) {
+		let msg = "";
 
-    if (error instanceof Error) {
-      msg = error.message;
-    }
-    throw new Error(kleur.red(makeError(msg)));
-  }
+		if (error instanceof Error) {
+			msg = error.message;
+		}
+		throw new Error(kleur.red(makeError(msg)));
+	}
 };
 
 export default fp<DirRoutesPluginOptions>(DirRoutesPlugin);
