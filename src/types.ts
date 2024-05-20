f216bae5c5a0ef4f287bdd4a037f5a53601edbed
Franklin Shera <fshera96@gmail.com>
2024-05-20 18:12:33 +0300

    chore: add jsdoc

--- src/types.ts
+++ src/types.ts
@@ -1,60 +1,70 @@
 import type {
-  ContextConfigDefault,
-  FastifyInstance,
-  FastifyReply,
-  FastifyRequest,
-  RawReplyDefaultExpression,
-  RawRequestDefaultExpression,
-  RawServerBase,
-  RawServerDefault,
-  RouteShorthandOptions,
+	ContextConfigDefault,
+	FastifyInstance,
+	FastifyReply,
+	FastifyRequest,
+	RawReplyDefaultExpression,
+	RawRequestDefaultExpression,
+	RawServerBase,
+	RawServerDefault,
+	RouteShorthandOptions,
 } from "fastify";
 
 import type { RouteGenericInterface } from "fastify/types/route";
 
 type DirRouteHandlerMethod<
-  RawServer extends RawServerBase = RawServerDefault,
-  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
-  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>,
-  RouteGeneric extends RouteGenericInterface = RouteGenericInterface,
-  ContextConfig = ContextConfigDefault
+	RawServer extends RawServerBase = RawServerDefault,
+	RawRequest extends
+		RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
+	RawReply extends
+		RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>,
+	RouteGeneric extends RouteGenericInterface = RouteGenericInterface,
+	ContextConfig = ContextConfigDefault,
 > = (
-  this: FastifyInstance<RawServer, RawRequest, RawReply>,
-  request: FastifyRequest<RouteGeneric, RawServer, RawRequest>,
-  reply: FastifyReply<
-    RawServer,
-    RawRequest,
-    RawReply,
-    RouteGeneric,
-    ContextConfig
-  >,
-  server: FastifyInstance<RawServer, RawRequest, RawReply>
-  // biome-ignore lint/suspicious/noConfusingVoidType: <explanation>
+	this: FastifyInstance<RawServer, RawRequest, RawReply>,
+	request: FastifyRequest<RouteGeneric, RawServer, RawRequest>,
+	reply: FastifyReply<
+		RawServer,
+		RawRequest,
+		RawReply,
+		RouteGeneric,
+		ContextConfig
+	>,
+	server: FastifyInstance<RawServer, RawRequest, RawReply>,
+	// biome-ignore lint/suspicious/noConfusingVoidType: <explanation>
 ) => void | RouteGeneric["Reply"] | Promise<RouteGeneric["Reply"] | void>;
 
 export interface RouteHandler<
-  RouteGeneric extends RouteGenericInterface = RouteGenericInterface,
-  ContextConfig = ContextConfigDefault,
-  RawServer extends RawServerBase = RawServerDefault,
-  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
-  RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>
+	RouteGeneric extends RouteGenericInterface = RouteGenericInterface,
+	ContextConfig = ContextConfigDefault,
+	RawServer extends RawServerBase = RawServerDefault,
+	RawRequest extends
+		RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
+	RawReply extends
+		RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>,
 > extends DirRouteHandlerMethod<
-    RawServer,
-    RawRequest,
-    RawReply,
-    RouteGeneric,
-    ContextConfig
-  > {
-  opts?: RouteShorthandOptions<
-    RawServer,
-    RawRequest,
-    RawReply,
-    RouteGeneric,
-    ContextConfig
-  >;
+		RawServer,
+		RawRequest,
+		RawReply,
+		RouteGeneric,
+		ContextConfig
+	> {
+	opts?: RouteShorthandOptions<
+		RawServer,
+		RawRequest,
+		RawReply,
+		RouteGeneric,
+		ContextConfig
+	>;
 }
 
 export type DirRoutesPluginOptions = {
-  routesDir: string;
-  prefix?: string;
+	/**
+	 * Directory of routes
+	 */
+	routesDir: string;
+	/**
+	 * Prefix for all routes eg `/api`
+	 */
+	prefix?: string;
 };
